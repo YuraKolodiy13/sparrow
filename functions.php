639c28caf2b271707a53800ba01691225e7075b9
Timber <Macroch@yandex.ru>
2018-06-25 23:43:00 +0300

    initial

---
+++ functions.php
@@ -0,0 +1,22 @@
+
+<?php
+
+add_action ('wp_enqueue_scripts', 'theme_styles');
+add_action ('wp_enqueue_scripts', 'theme_scripts');
+
+function theme_styles(){
+    wp_enqueue_style('default', get_template_directory_uri() . '/assets/css/default.css');
+    wp_enqueue_style('fonts', get_template_directory_uri() . '/assets/css/fonts.css');
+    wp_enqueue_style('layout', get_template_directory_uri() . '/assets/css/layout.css');
+    wp_enqueue_style('media', get_template_directory_uri() . '/assets/css/media-queries.css');
+}
+
+function theme_scripts(){
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', '//ajax/libs/jquery/1.10.2/jquery.min.js');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js', ['jquery'], null, true);
+    wp_enqueue_script('init', get_template_directory_uri() . '/assets/js/init.js', ['jquery'], null, true);
+    wp_enqueue_script('doubletaptogo', get_template_directory_uri() . '/assets/js/doubletaptogo.js', ['jquery'], null, true);
+    wp_enqueue_script('modernizr', get_template_directory_uri() . '/assets/js/modernizr.js', null, null, false);
+}
